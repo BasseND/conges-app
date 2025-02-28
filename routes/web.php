3fa83ef6b824c567a38423bd713281c6de765a4b
BasseND <ndiayebasse@gmail.com>
2025-02-28 20:38:27 +0100

    Add fortify

--- routes/web.php
+++ routes/web.php
@@ -62,22 +62,7 @@ Route::middleware('guest')->group(function () {
 
 // Routes protégées par l'authentification
 Route::middleware('auth')->group(function () {
-    // Vérification d'email
-    Route::get('verify-email', EmailVerificationPromptController::class)
-                ->name('verification.notice');
-
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['signed', 'throttle:6,1'])
-                ->name('verification.verify');
-
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware('throttle:6,1')
-                ->name('verification.send');
-
     // Mot de passe et déconnexion
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->name('password.confirm');
-
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
