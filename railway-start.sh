8b9ad67a869b922fbb929412d1c48c1c61626bd3
BasseND <ndiayebasse@gmail.com>
2025-02-28 15:54:00 +0100

    Config key gen

--- railway-start.sh
+++ railway-start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Attendre que MySQL soit prêt
+echo "Waiting for MySQL to be ready..."
+while ! mysqladmin ping -h"$MYSQLHOST" -P"$MYSQLPORT" -u"$MYSQLUSER" -p"$MYSQLPASSWORD" --silent; do
+    sleep 1
+done
+echo "MySQL is ready!"
+
 # Copier le fichier d'environnement
 cp .env.railway .env
-
-# Générer la clé d'application
-php artisan key:generate --force
 
 # Migrations
 php artisan migrate --force
