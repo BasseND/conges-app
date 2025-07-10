aeef5b91da5ac3f2184ce77b394fdc78a4d77e93
bndiaye1 <bndiaye1-ext@mgen.fr>
2025-07-10 22:36:22 +0200

    Fix: Correction de l'ordre des migrations pour la colonne gender

--- database/migrations/2025_05_02_191429_add_gender_to_users_table.php
+++ database/migrations/2025_05_02_191429_add_gender_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('gender', ['M', 'F'])->nullable()->after('name');
+            $table->enum('gender', ['M', 'F'])->nullable()->after('first_name');
         });
     }
 
