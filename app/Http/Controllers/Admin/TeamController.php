34284a02cb25e3c9d7437313aca53c1bd037d8b6
bndiaye1 <bndiaye1-ext@mgen.fr>
2025-07-16 00:53:40 +0200

    Bug fix

--- app/Http/Controllers/Admin/TeamController.php
+++ app/Http/Controllers/Admin/TeamController.php
@@ -171,7 +171,7 @@ class TeamController extends Controller
         Log::info('Récupération des équipes pour le département: ' . $department->id);
         
         try {
-            $teams = $department->teams()->with('manager:id,name')->get();
+            $teams = $department->teams()->with('manager:id,first_name,last_name')->get();
             Log::info('Équipes trouvées: ' . $teams->count(), ['teams' => $teams->toArray()]);
             
             return response()->json($teams);
